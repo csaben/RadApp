a596ef696031e359a75b15ba8c65ff800596caee
arelius <76020733+csaben@users.noreply.github.com>
2021-11-06 16:53:04 -0400

    Update inference.py

--- arch_ml/inference.py
+++ arch_ml/inference.py
@@ -1,13 +1,10 @@
+#Sample Inference Script
+
 #preprocessing function. Parameters (CID file location, label)
-
-
-
 
 
 #call model from folder and conduct inference
 
 
-
-
-
 #return label, % confidence
+
