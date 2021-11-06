2a183b394a674b33c6f1aa8f2d5dc1c99c1713e2
arelius <76020733+csaben@users.noreply.github.com>
2021-11-06 16:45:38 -0400

    Create visualize.py

---
+++ dcexample_code/visualize.py
@@ -0,0 +1,15 @@
+#imports
+import pydicom
+from pydicom.pixel_data_handlers.util import apply_color_lut
+from pydicom import dcmread
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+#grab the pixel array data from the dcm
+fname = "Image-105.dcm" #insert your relative dcm file
+ds = dcmread(fname)
+arr = ds.pixel_array
+
+#interpolate the nd.array storing the pixel array as an image and display on screen
+plt.imshow(arr, interpolation='nearest')
+plt.show()
